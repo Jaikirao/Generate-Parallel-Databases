ba5c4d0ac9fe392b7402aa48171bcc2a131df19b
Jaikirao <48488555+Jaikirao@users.noreply.github.com>
2019-06-22 19:01:24 +0530

    Update parallel_db.py

--- parallel_db.py
+++ parallel_db.py
@@ -1,1 +1,16 @@
 print("i start this project I will complete this project soon")
+#try here
+import torch
+
+# the number of entries in our database
+num_entries = 5000
+
+db = torch.rand(num_entries) > 0.5
+db
+#create a fuction of parallel database
+def get_parallel_db(db , remove_index):       #remove_index to remove the entry
+  return torch.cat((db[0:remove_index],
+                    db[remove_index+1:]))
+#now call this fuction
+get_parallel_db(db,2)
+#now we create a another fuction get_parralle_dbs after making create we combine both the fuction
